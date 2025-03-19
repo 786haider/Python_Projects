ff2dbc7dbbf100a17c8510e89c57ac51a07af657
786haider <haiderhussain536@gmail.com>
2025-03-20 04:31:49 +0500

    second

---
+++ 00_intro_python/02_agreement_bot/app.py
@@ -0,0 +1,7 @@
+def main():
+    user_fav_animal = input(f"Whats your favoruite animal? ")
+    my_fav_animal = user_fav_animal
+    print(f" Ohh wao! my favorite animal is also {my_fav_animal}.")
+    
+if __name__ == "__main__":
+    main()
---
+++ 00_intro_python/02_agreement_bot/readme.md
@@ -0,0 +1,5 @@
+# 02_agreement_bot
+This is a simple agreement bot that agrees with user's input.
+python
+To run this code, 
+# python app.py
