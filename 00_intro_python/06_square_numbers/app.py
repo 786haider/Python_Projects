b502f45dd8138ffdc6a815b20c8cc4d9e872c72d
786haider <haiderhussain536@gmail.com>
2025-03-21 20:47:00 +0500

    00_intro_python complete

---
+++ 00_intro_python/06_square_numbers/app.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hello World ! Enter the number to find the square number")
+    user_number = int(input("Enter your number : "))
+    print(f"The square of {user_number} is {user_number**2}")
+    
+if __name__ == "__main__":
+    main()
---
+++ 00_intro_python/06_square_numbers/readme.md
@@ -0,0 +1,2 @@
+# app,py
+This app is used to calculate the square of a number.
