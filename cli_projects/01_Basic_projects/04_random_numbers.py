b4683f963e50cd1c065175112a68722d18357379
786haider <haiderhussain536@gmail.com>
2025-04-12 02:48:43 +0500

    basic complete

---
+++ cli_projects/01_Basic_projects/04_random_numbers.py
@@ -0,0 +1,13 @@
+import random
+
+N_NUMBERS: int = 10
+MIN_VALUE: int = 1
+MAX_VALUE: int = 100
+
+def main():
+    value: list[int] = [random.randint(MIN_VALUE, MAX_VALUE)
+    for _ in range(N_NUMBERS)]
+    print(value)
+
+if __name__ == '__main__':
+    main()
