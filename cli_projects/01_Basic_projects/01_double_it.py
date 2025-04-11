5a10eb7e7d0718ec17f6fe6f955e0b89c58233e0
786haider <haiderhussain536@gmail.com>
2025-04-12 02:32:47 +0500

    01_double_it

---
+++ cli_projects/01_Basic_projects/01_double_it.py
@@ -0,0 +1,11 @@
+def main():
+    user_input = input("Enter a number to double till it reaches 100: ")
+    num = int(user_input)
+    while num < 100:
+        num = num * 2
+        print(num)
+if __name__ == "__main__":
+    main()
+    
+    
+    
